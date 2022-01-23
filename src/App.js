a350ab523b47362d4dc0caafd86d5218b62164d0
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2022-01-23 18:54:22 +0330

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+
+import "../Styles/Header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h3>Plastic is an award-winning international agency.</h3>
+      <h1>We create remarkable</h1>
+      <h1>
+        <Typewriter
+          loop
+          typeSpeed={70}
+          deleteSpeed={60}
+          delaySpeed={2000}
+          words={[
+            "digital products.",
+            "digital-first brands.",
+            "digital experiences",
+          ]}
+        />
+      </h1>
+      <button>Our showreel</button>
+    </div>
+  );
+};
+
+export default Header;
