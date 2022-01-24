8b0604fb66a94198f174e6aab8b804947af3cea8
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2022-01-24 09:59:56 +0330

    Create main Body and make it responsive

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import { CustomCursor } from "./components/CustomCursor";
+import Body from "./components/Body";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
       <CustomCursor />
       <Navbar />
       <Header />
+      <Body />
     </div>
   );
 }
